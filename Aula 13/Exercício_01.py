25116ca357d0ff27a5dc78ae32f39a71c5c457b9
Fernando Marques <50237699+FeMarqs@users.noreply.github.com>
2024-11-27 11:16:48 -0300

    Estrutura de Dados

--- Aula 13/Exercício_01.py
+++ Aula 13/Exercício_01.py
@@ -4,5 +4,17 @@ system('cls')
 #Criar uma lista de números. Crie uma lista chamada números 
 #contendo os números 10 a 20 (inclusive). Imprima a lista.
 
-numeros = [10, 11, 12, 13, 14, 15, 16, 17 , 18 , 19 , 20]
-print(numeros)
+""" numeros = [10, 11, 12, 13, 14, 15, 16, 17 , 18 , 19 , 20]
+print(numeros) """
+
+numeros = []
+
+for i in range(10, 21):
+    numeros.append(i) #adciona o número à lista 
+    print(i) #imprime o número durante o loop
+    
+#após o loop, imprime a lista inteira
+print("Lista completa de números de 10 a 20:", numeros)
+
+
+
---
+++ Aula 13/Exercício_02.py
@@ -0,0 +1,11 @@
+#Acessar o último elemento de uma lista
+#Crie uma lista chamada frutas com os seguintes elementos:["banana", "maçã", "laranja", "manga"] 
+#Acesse e imprima o último item da lista.
+from os import system
+system('cls')
+
+frutas = ["banana", "maçã", "laranja", "manga"]
+print(frutas)
+print('-_-'*36)
+
+print(frutas[-1])
