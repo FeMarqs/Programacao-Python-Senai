cb358a063a2bf3ff35a69d316232145e2289de52
Fernando Marques <50237699+FeMarqs@users.noreply.github.com>
2024-11-27 10:43:57 -0300

    Estrutura de Dados

---
+++ Aula 13/Exercício_01.py
@@ -0,0 +1,8 @@
+from os import system
+system('cls')
+
+#Criar uma lista de números. Crie uma lista chamada números 
+#contendo os números 10 a 20 (inclusive). Imprima a lista.
+
+numeros = [10, 11, 12, 13, 14, 15, 16, 17 , 18 , 19 , 20]
+print(numeros)
