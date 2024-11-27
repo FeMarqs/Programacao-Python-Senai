6b14f8555f008c500340a39c0519f9547bb537d8
Fernando Marques <50237699+FeMarqs@users.noreply.github.com>
2024-11-27 11:44:50 -0300

    Estrutura de Dados

---
+++ Aula 13/Exercício_03.py
@@ -0,0 +1,11 @@
+#Modificar um elemento da lista
+#Dada a lista frutas = ["banana", "maçã", "laranja"], altere o segundo item para "Kiwi". 
+#Em seguida, imprima a lista modificada
+
+from os import system
+system('cls')
+
+frutas = ["banana", "maça", "laranja"]
+
+frutas[1] = "kiwi"
+print(frutas)
---
+++ Aula 13/Exercício_04.py
@@ -0,0 +1,12 @@
+#Adcionar um item no final da lista
+#Crie uma lista cores com os seguintes elementos:["azul","vermelho","verde"].
+#Em seguida, imprima a lista modificada.
+
+from os import system
+system('cls')
+
+cores = ["azul", "vermelho", "verde"]
+print(cores)
+
+cores.append("amarelo")
+print(cores)
